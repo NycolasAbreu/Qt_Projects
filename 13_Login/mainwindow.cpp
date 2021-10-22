585037ecee40fb01f2dd2a96e5ffff635ebc7401
Nycolas Coelho de Abreu <nyco_abreu@hotmail.com>
2021-10-22 16:42:53 -0300

    Push

--- 13_Login/mainwindow.cpp
+++ 13_Login/mainwindow.cpp
@@ -1,15 +1,36 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    ui->lineEdit_Senha->setEchoMode(QLineEdit::Password);       // Esconde a senha
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+
+    QString login = ui->lineEdit_Login->text();     // Salva na variavel o dado
+    QString senha = ui->lineEdit_Senha->text();
+
+    if(login == "Nycolas" && senha == "1234")
+        QMessageBox::information(this,"Login","Login Correto");
+    else
+        QMessageBox::information(this,"Login","Login Errado");
+
+
+
+    ui->lineEdit_Login->clear();        // Limpa a linha
+    ui->lineEdit_Senha->clear();
 }
 
